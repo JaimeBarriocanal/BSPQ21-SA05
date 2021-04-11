b16adf737338338078dccbd505d4441c0a35d809
ggarate12 <gorka.garate@opendeusto.es>
2021-04-11 19:15:40 +0200

    .

---
+++ deusto-videoclub/src/main/java/es/deusto/spq/grupoA05/deusto_videoclub/App.java
@@ -0,0 +1,13 @@
+package es.deusto.spq.grupoA05.deusto_videoclub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
