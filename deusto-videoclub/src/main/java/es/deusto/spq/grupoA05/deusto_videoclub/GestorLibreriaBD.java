d87735a9aef501b00a21f75c536f4828b4fb081d
ggarate12 <gorka.garate@opendeusto.es>
2021-06-23 18:00:33 +0200

    Eliminados imports inutiles

--- deusto-videoclub/src/main/java/es/deusto/spq/grupoA05/deusto_videoclub/GestorLibreriaBD.java
+++ deusto-videoclub/src/main/java/es/deusto/spq/grupoA05/deusto_videoclub/GestorLibreriaBD.java
@@ -1,7 +1,5 @@
 package es.deusto.spq.grupoA05.deusto_videoclub;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
